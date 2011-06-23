3bed015d2c4b4bac3a1aaaf7054daff0196e09c0
cesao <cesar.consultorjr@gmail.com>
2011-06-23 16:24:35 -0300

    classe java adicionada

--- src/br/com/dimb/exercite/Home.java
+++ src/br/com/dimb/exercite/Home.java
@@ -9,5 +9,6 @@ public class Home extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //aqui vai mais um teste
     }
 }
